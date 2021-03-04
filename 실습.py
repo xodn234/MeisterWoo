25c674319def7909a2e1c31c5a124a6b9632d847
xodn234 <78893090+xodn234@users.noreply.github.com>
2021-03-05 08:55:26 +0900

    Add files via upload

--- 실습.py
+++ 실습.py
@@ -1,3 +1,82 @@
+## 사이트 비밀번호
+
+# url = "http//:google.co.kr"
+# site = url.replace("http//:","")
+# site = site[:site.index(".")]
+# password = site[0:3] + str(len(site)) + str(site.count("e"))
+# print(password)
+
+
+## 오늘의 운세
+
+# import random
+# solution = [
+#     "좋은일이 생길거야",
+#     "조심하렴",
+#     "그저그런 하루",
+#     ]
+# today = random.randint(0, len(solution)-1)
+# print(solution[today])
+
+
+## 총점과 평균 구하기
+
+# subject = ["korean", "english", "math"]
+# point = [7, 8, 9]
+# total = 0
+# avr = 0
+# points = point.copy()
+# while points:
+#     total += points.pop()
+#     avr = total / len(point)
+# print(total, avr)
+
+
+## 늘어나는 숫자
+
+# c= 0
+# for a in range(10):
+#     c += 1
+#     for b in range(c):
+#         print(b, end = " ")
+#     print()
+
+
+## 구구단 출력
+
+# for a in range(1, 10):
+#     for b in range(1, 10):
+#         print("{}X{}={}".format(b, a, a*b), end = "\t")
+#     print()
+
+
+## 로또번호
+
+# import random
+# lotto = []
+# while len(lotto) <= 5:
+#     numbers = random.randint(1, 45)
+#     if numbers not in lotto:
+#         lotto.append(numbers)
+#         number = len(lotto) - 1
+#         print("당첨번호 : " + str(lotto[number]))
+# lotto.sort()
+# print("최종번호 : " + str(lotto))
+
+
+## 연금복권 번호
+
+# import random
+# number = random.randint(1, 5)
+# lotto = []
+# while len(lotto) <= 5:
+#     numbers = random.randint(0, 9)
+#     lotto.append(numbers)
+# print("{}조".format(number), lotto)
+
+
+## 알파벳 카운트
+
 # text = "aineapple"
 # dic = {word:text.count(word) for word in text}
 # print(dic)
